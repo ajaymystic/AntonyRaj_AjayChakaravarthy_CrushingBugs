f45945d9b25aa5be5845c0dd278b40e62ccd0a1e
Ajay Chakaravarthy Antony Raj <ajaychakaravarthy22@gmail.com>
2025-07-01 21:18:32 -0400

    Updated Js
    In this commit, fixed the reset button

--- js/main.js
+++ js/main.js
@@ -29,22 +29,37 @@ function dragLeave(event) {
 
 function drop(event) {
     event.preventDefault();
-  
+
 //Double drop logic
-this.classList.remove("drag-over");
+    this.classList.remove("drag-over");
 
     if (this.children.length > 0) return;
 
     this.appendChild(currentDraggedElement);
-   
 
 //  Matching the correct labels 
+    const labelText = currentDraggedElement.textContent.trim().toLowerCase();
+    const targetId = this.id.toLowerCase();
 
+    if (labelText === targetId) {
+        this.classList.add("correct-drop");
+    } else {
+        this.classList.add("wrong-drop");
+    }
+    currentDraggedElement = null;
+}
 
 //Reset Btn 
 function resetGame() {
     console.log("Reset Game called");
 
+    targetZones.forEach(zone => {
+        if (zone.children.length > 0) {
+            const label = zone.firstElementChild;
+            labelBox.appendChild(label);
+        }
+        zone.classList.remove("correct-drop", "wrong-drop", "drag-over"); 
+    });
 }
 
 // Event Listeners
@@ -54,8 +69,9 @@ labels.forEach(label => {
 
 targetZones.forEach(target => {
     target.addEventListener("dragover", dragOver);
- 
+    target.addEventListener("dragenter", dragEnter);
+    target.addEventListener("dragleave", dragLeave);
     target.addEventListener("drop", drop);
 });
 
-}
+resetBtn.addEventListener("click", resetGame);
